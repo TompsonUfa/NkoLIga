eb015e5dab1d32575d4b149aa3f1fa2f7a4b89d8
Roman <romanrus98@mail.ru>
2023-12-27 23:37:24 +0500

    contact

--- resources/js/src/components/SectionFeedback.vue
+++ resources/js/src/components/SectionFeedback.vue
@@ -3,14 +3,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <div class="feedback__content">
-                        <h4 class="feedback__title">Свяжитесь с нами</h4>
+                    <div class="feedback__title">
+                        <h4>Свяжитесь с нами</h4>
                     </div>
                 </div>
                 <div class="col-12">
-                   <div class="row">
-                       <div class="col-12 col-md-6 m-auto">
-                           <form class="feedback__form p-md-5" action="">
+                   <div class="row feedback__content">
+                       <div class="col-12 col-md-6 p-md-0">
+                           <form class="feedback__form p-4 p-md-5 h-100" action="">
                                <div class="mb-3">
                                    <label for="name" class="form-label">Имя:</label>
                                    <input type="text" class="form-control" id="name">
@@ -25,10 +25,15 @@
                                </div>
                                <div class="mb-3">
                                    <label for="text" class="form-label">Сообщение:</label>
-                                   <textarea name="text" class="form-control"></textarea>
+                                   <textarea name="text" class="form-control" ></textarea>
                                </div>
-                               <button class="btn btn-primary feedback__btn">Отправить</button>
+                               <div class="w-100 text-center">
+                                   <button class="btn btn-primary feedback__btn">Отправить</button>
+                               </div>
                            </form>
+                       </div>
+                       <div class="col-12 col-md-6 p-md-0 d-none d-md-block">
+                           <iframe class="feedback__map" src="https://yandex.ru/map-widget/v1/?um=constructor%3Acf6f5836d26e5e64a59dffd541bbbfd6af2e536d0ace1e5978308912f118e1b6&amp;source=constructor" frameborder="0"></iframe>
                        </div>
                    </div>
                 </div>
@@ -71,27 +76,43 @@ export default {
             content: "";
         }
         &__form{
-            //width: 60%;
-            margin: 0 auto 50px;
             position: relative;
             z-index: 1;
-            color: #fff;
             background-color: #fff;
-            border-radius: 15px;
+            border-bottom-left-radius: 5px;
+            border-top-left-radius: 5px;
+            color: #0d0d0d;
+            input, textarea {
+                &:focus {
+                    border-color: var(--second-color);
+                    box-shadow: none;
+                }
+            }
+            textarea {
+                height: 200px;
+                resize: none;
+            }
         }
         &__content {
-            text-align: center;
-            width: 60%;
+            width: 100%;
             margin: 0 auto 50px;
             position: relative;
             z-index: 1;
             color: #fff;
+        }
+        &__map {
+            border: 1px solid #fff;
+            width: 100%;
+            height: 100%;
         }
         &__title {
             position: relative;
+            text-align: center;
             padding-bottom: 24px;
             margin-bottom: 20px;
             font-weight: 700;
+            z-index: 1;
+            color: #fff;
             &:after {
                 position: absolute;
                 content: "";
@@ -102,15 +123,6 @@ export default {
                 right: 0;
                 margin: 0 auto;
                 bottom: 0;
-            }
-        }
-        &__form {
-            color: #0d0d0d;
-            input, textarea {
-                &:focus {
-                    border-color: var(--second-color);
-                    box-shadow: none;
-                }
             }
         }
         &__btn {
