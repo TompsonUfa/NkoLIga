a1e7e2f160bfdfd469cd5f99af4305815d3249b7
TompsonUfa <86980436+TompsonUfa@users.noreply.github.com>
2024-01-17 16:26:18 +0500

    SEO

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
-    <title>LIGA</title>
+    <meta name="description"
+          content="АНО Центр образования, науки, физической культуры, спорта и здоровья «Лига»
+           осуществляет научную и образовательную деятельность в сфере физической культуры, спорта и здоровья.
+           Также занимается организацией физкультурно-спортивных,
+          физкультурно-оздоровительных, спортивно- и культурно-зрелищных и досуговых мероприятий.">
+    <meta name="Keywords"
+          content="Организация лекций, Организация семинаров, Организация конференций, Организация тренингов,
+          Организация совещаний, Организация
+          круглых столов, Организация симпозиумов,АНО Лига,Лига,LIGA">
+    <link rel='shortcut icon' type='image/x-icon' href='/public/favicon.ico' />
+    <title>Лига</title>
     @vite(['resources/js/app.js', 'resources/sass/app.scss'])
 </head>
 <body>
